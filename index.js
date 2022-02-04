ad5a5a24854717dc4c3897829c629491441f2a25
adamcg123 <adam.Gondorchin26@gmail.com>
2022-02-04 11:52:39 -0600

    got the first test working

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const generateTeam = require("./src/teamGenerate")
+
+teamArray = [];
